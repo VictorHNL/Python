6c75a69c2d6d8b5385854e9b4367501eb962b570
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:56:20 -0300

    Exercicio11.py

--- Lista de exercícios Python/Exercicio11.py
+++ Lista de exercícios Python/Exercicio11.py
@@ -1,3 +1,4 @@
+#11. Escreva um programa que declare uma constante (PI) e calcule a área de um círculo com raio fornecido pelo usuário.
 pi = 3.14159265359
 raio = float(input("raio: "))
 area_circulo = ((raio * raio) * pi)
