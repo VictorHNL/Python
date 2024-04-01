49dfa33ee8cf686ed2b97df1a02b4a2bc0522b38
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:59:33 -0300

    Exercicio8.py

--- Lista de exercícios Python/Exercicio8.py
+++ Lista de exercícios Python/Exercicio8.py
@@ -1,3 +1,4 @@
+#8. Escreva um programa que converta um valor em dólares para reais. O usuário deve informar a taxa de câmbio e o valor em dólares.
 dolar = float(input("Digite seu valor em dólares:"))
 taxa_de_cambio = ("R$4.94 para cada 1.0US$")
 print(taxa_de_cambio)
