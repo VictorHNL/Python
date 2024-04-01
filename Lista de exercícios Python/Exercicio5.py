647e21f81aa59d4ce464e1e00637b587a1eaf81a
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:58:29 -0300

    Update Exercicio5.py

--- Lista de exercícios Python/Exercicio5.py
+++ Lista de exercícios Python/Exercicio5.py
@@ -1,3 +1,5 @@
+#5. Escreva um programa que calcule e exiba a média de três notas inseridas pelo usuário.
+
 nota1 = float(input("Nota 1: "))
 nota2 = float(input("Nota 2: "))
 nota3 = float(input("Nota 3: "))
