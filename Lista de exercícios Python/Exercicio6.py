0cbd5e4deb9dafa87c8ec3a89a24b69c83543ad9
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:58:50 -0300

    Exercicio6.py

--- Lista de exercícios Python/Exercicio6.py
+++ Lista de exercícios Python/Exercicio6.py
@@ -1,3 +1,5 @@
+#6. Desenvolva um programa que calcule o volume de uma esfera com base no raio fornecido pelo usuário.
+
 raio = float(input("Raio: "))
 volume = ((4 * 3.14159265358979323846 * raio) / 3)
 print(f"Volume: {volume}")
