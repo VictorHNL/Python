5f758b0ee3124c3132b3601a7b7422a4b3fe509f
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:58:10 -0300

    Update Exercicio4.py

--- Lista de exercícios Python/Exercicio4.py
+++ Lista de exercícios Python/Exercicio4.py
@@ -1,3 +1,4 @@
+#4. Crie um programa que converta a temperatura de Celsius para Fahrenheit. Peça ao usuário para inserir a temperatura em Celsius.
 print("Programa para converção de Graus para Fahrenheit")
 graus = float(input("Graus: "))
 converter = ((graus * 9/5) + 32)
