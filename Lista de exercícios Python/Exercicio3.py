149ba6ac8476d7202668d0388ee4aa141f25f640
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:57:45 -0300

    Update Exercicio3.py

--- Lista de exercícios Python/Exercicio3.py
+++ Lista de exercícios Python/Exercicio3.py
@@ -1,3 +1,5 @@
+#3. Desenvolva um programa que peça ao usuário para inserir dois números e exiba a mulFplicação deles
+
 n1 = int(input("Escreva um numero para sua multiplicação: "))
 n2 = int(input("Escreva agora outro número: "))
 print(f"A multiplicação de {n1} e {n2} foi de : {n1 * n2}")
