3da36c28081e3b86c606831c497147ddde6bf6d0
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:59:58 -0300

    Exercicios9.py

--- Lista de exercícios Python/Exercicios9.py
+++ Lista de exercícios Python/Exercicios9.py
@@ -1,3 +1,4 @@
+#9. Desenvolva um programa que calcule o quadrado de um número inserido pelo usuário.
 quadrado = float(input("Escreva o numero na qual você deseja elevar ao quadrado: "))
 elevado = (quadrado * quadrado)
 print(f"Seu número é {elevado}")
