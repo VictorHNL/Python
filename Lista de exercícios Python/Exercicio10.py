2a4da640f547c68d93274464434166d6418d32d8
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:55:52 -0300

    Exercicio10.py

--- Lista de exercícios Python/Exercicio10.py
+++ Lista de exercícios Python/Exercicio10.py
@@ -1,3 +1,4 @@
+#10. Crie um programa que solicite dois números e exiba o resultado da divisão do primeiro pelo segundo.
 n1 = float(input("numero 1: "))
 n2 = float(input("numero 2: "))
 divisao = (n1 / n2)
