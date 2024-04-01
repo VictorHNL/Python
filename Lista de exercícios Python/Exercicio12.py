ece6a3093ab02e6826f1e6d6cd876c078092f5a8
Victor Hugo <163074223+VictorHNL@users.noreply.github.com>
2024-04-01 16:56:43 -0300

    Exercicio12.py

--- Lista de exercícios Python/Exercicio12.py
+++ Lista de exercícios Python/Exercicio12.py
@@ -1,3 +1,4 @@
+#12. Desenvolva um programa que peça o nome e a idade do usuário e exiba uma mensagem com o nome e o ano de nascimento.
 nome = input("Digite seu nome: ")
 idade = input("Digite sua idade: ")
 
